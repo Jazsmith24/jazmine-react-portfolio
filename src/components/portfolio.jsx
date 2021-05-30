e5f0c5aec0ff36b525b419df6df835d8a8f3d532
jaz <jazminetsmith@gmail.com>
2021-05-30 14:02:57 -0500

    Added Drag me please project

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -8,8 +8,12 @@ import horter4 from "../img/Horter/garden_info.jpg";
 import horter5 from "../img/Horter/plant_search.jpg";
 import horter from "../img/Horter/horter.png";
 import gardenView from "../img/Horter/Garden Settings.jpg";
-
-
+import dragme from "../img/DragMePlease Logo.png";
+import dragdemo from "../img/drag_me_please/Drag_me_please_demo.mp4";
+import dmp from "../img/drag_me_please/dmp.PNG";
+import dmp1 from "../img/drag_me_please/dmp1.png";
+import dmpflip from "../img/drag_me_please/dmpflip.png";
+import dmpflipcard from "../img/drag_me_please/dmpflipcard.png";
 
 class Portfolio extends React.Component {
   render() {
@@ -21,7 +25,8 @@ class Portfolio extends React.Component {
               <div className="title-box text-center">
                 <h3 className="title-a">Portfolio</h3>
                 <p className="subtitle-a">
-                  Below are photos of my most recent projects along with technologies/methodologies used to build them.
+                  Below are photos of my most recent projects along with
+                  technologies/methodologies used to build them.
                 </p>
                 <div className="line-mf"></div>
               </div>
@@ -40,7 +45,10 @@ class Portfolio extends React.Component {
                         <h2 className="w-title">Horter(PWA)</h2>
                         <div className="w-more">
                           <span className="w-ctegory">
-                           Vue.js Webpack.js Express Node.js PostgreSQL AWS HTML5 CSS3 Bootstrap Buefy Sass OAUTH Passport DialogueFlow GoogleMaps Babel Mocha/Chai(testing)
+                            <span><p><b>Horter is an application which helps users create gardens, update and maintain information about them, and learn more about how to add to them and keep them growing strong</b></p></span>
+                            Javascript Vue.js Webpack.js Express Node.js PostgreSQL AWS
+                            HTML5 CSS3 Bootstrap Buefy Sass OAUTH Passport
+                            DialogueFlow GoogleMaps Babel Mocha/Chai(testing)
                           </span>
                         </div>
                       </div>
@@ -94,29 +102,30 @@ class Portfolio extends React.Component {
                 >
                   jsx-a11y/anchor-has-content warning
                 </a>
-               <a
+                <a
                   href={horter5}
                   data-lightbox="gallery-vmarine"
                   style={{ display: "none" }}
                 >
                   jsx-a11y/anchor-has-content warning
-                </a> 
+                </a>
               </div>
             </div>
-            {/* <div className="col-md-4"> <--- add another project here
+            <div className="col-md-4">
               <div className="work-box">
-                <a href={stock} data-lightbox="gallery-aguadeluz">
+                <a href={dragme} data-lightbox="gallery-aguadeluz">
                   <div className="work-img">
-                    <img src={stock} alt="" className="img-fluid" />
+                    <img src={dragme} alt="" className="img-fluid" />
                   </div>
                   <div className="work-content">
                     <div className="row">
                       <div className="col-sm-8">
-                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <h2 className="w-title">Drag Me Please</h2>
                         <div className="w-more">
                           <span className="w-ctegory">
-                            HTML5 CSS3 Bootstrap Webpack SmoothScrolling
-                            VanillaJS
+                          <div><span><b>This is a front-end web app to keep up with the fabulous queens of RuPaul's Drag race. You will be able to go through each season and view all drag queens!</b></span></div>
+                          React HTML SSCC/SASS CSS Bootstrap Webpack Javascript
+                          Axios  
                           </span>{" "}
                         </div>
                       </div>
@@ -128,43 +137,44 @@ class Portfolio extends React.Component {
                     </div>
                   </div>
                 </a>
+
+                {/* <a
+                  href={dragme}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a> */}
                 <a
-                  href={stock1}
+                  href={dmp}
                   data-lightbox="gallery-aguadeluz"
                   style={{ display: "none" }}
                 >
                   jsx-a11y/anchor-has-content warning
                 </a>
                 <a
-                  href={stock2}
+                  href={dmp1}
                   data-lightbox="gallery-aguadeluz"
                   style={{ display: "none" }}
                 >
                   jsx-a11y/anchor-has-content warning
                 </a>
                 <a
-                  href={stock3}
+                  href={dmpflip}
                   data-lightbox="gallery-aguadeluz"
                   style={{ display: "none" }}
                 >
                   jsx-a11y/anchor-has-content warning
                 </a>
                 <a
-                  href={stock4}
+                  href={dmpflipcard}
                   data-lightbox="gallery-aguadeluz"
                   style={{ display: "none" }}
                 >
                   jsx-a11y/anchor-has-content warning
                 </a>
-                <a
-                  href={stock5}
-                  data-lightbox="gallery-aguadeluz"
-                  style={{ display: "none" }}
-                >
-                  jsx-a11y/anchor-has-content warning
-                </a>
-              </div> */}
-            {/* </div> */}
+              </div>
+            </div>
           </div>
         </div>
       </section>
